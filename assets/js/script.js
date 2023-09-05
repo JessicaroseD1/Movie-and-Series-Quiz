e917a0f3607340ec9389896a874bbc61d9ee3f4b
JessicaroseD1 <>
2023-09-05 19:34:59 +0000

    Add constant for start and next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+
+startButton.addEventListener('click', main);
+
 function main() {
 
 }
 
 function setNextQuestion() {
-    
+
+}
+
+function selectAnswer() {
+
 }
