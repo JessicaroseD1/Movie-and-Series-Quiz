bdf71f58babd48bfb0333c09d74e92383e600964
JessicaroseD1 <>
2023-09-05 19:45:33 +0000

    Add array of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ startButton.addEventListener('click', main);
 function main() {
     startButton.classList.add('hide');
     questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 
 }
 
@@ -17,3 +18,34 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'In The Matrix, does Neo take the blue pill or the red pill',
+        answers: [
+            { text: 'red', correct: true },
+            { text: 'blue', correct: false }
+        ]
+    },
+    {
+        question: 'In the film Mean Girls, what day do they wear pink?',
+        answers: [
+            { text: 'Wednesday', correct: true },
+            { text: 'Thursday', correct: false },
+        ]
+    },
+    {
+        question: 'What DC movie was shockingly canceled by Warner Bros. in 2022?',
+        answers: [
+            { text: 'batgirl', correct: true },
+            { text: 'batman', correct: false },
+        ]
+    },
+    {
+        question: 'What is the first rule of Fight Club??',
+        answers: [
+            { text: 'eat nothing but chicken', correct: false },
+            { text: 'You do not talk about Fight Club.', correct: true },
+        ]
+    }
+];
