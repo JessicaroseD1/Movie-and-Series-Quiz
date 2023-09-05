37400fcfba84f6043272ba2131d7d37611ad98ed
JessicaroseD1 <>
2023-09-05 19:40:00 +0000

    Add constant to question container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
 
 startButton.addEventListener('click', main);
 
 function main() {
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
 
 }
 
