18491a115469955932b8a810236a7f46454acf10
JessicaroseD1 <>
2023-09-06 05:57:22 +0000

    Fix the next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ let shuffledQuestions, currentQuestionIndex;
 
 
 startButton.addEventListener('click', main);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
 
 function main() {
     startButton.classList.add('hide');
@@ -60,6 +65,21 @@ function selectAnswer(e) {
         endButton.classList.remove('hide');
     }
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
 
 const questions = [
     {
